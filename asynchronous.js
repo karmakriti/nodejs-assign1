1f8f5bdf1d997e03efb64ab412e7920f7beabbfc
Kriti211 <148147756+Kriti211@users.noreply.github.com>
2025-02-02 12:25:25 -0500

    First commit

---
+++ asynchronous.js
@@ -0,0 +1,16 @@
+
+const fs = require('fs');
+
+
+// Read file Asynchronously i.e non-blocking
+
+fs.readFile('./file.txt', 'utf8', (error, data) => {
+    if (error) {
+        console.log("Error reading file : " + error);
+        return; // Exits the callback function
+    }
+// print data
+    console.log('File data:\n', data);
+   
+});
+
